9a1779ab8a96bdfc074c39ea511a0215992b450d
efsiqueira <edu.fss14@gmail.com>
2022-04-28 16:16:52 -0300

    Atualização do banco de dados

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_28_125840) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_28_173037) do
+  create_table "enemies", force: :cascade do |t|
+    t.string "name"
+    t.integer "power_base"
+    t.integer "power_step"
+    t.integer "level"
+    t.integer "kind"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
