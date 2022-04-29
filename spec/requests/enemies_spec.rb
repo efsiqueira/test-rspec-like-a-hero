ba3c30cf2bdc91169707caab24321c390c978eed
efsiqueira <edu.fss14@gmail.com>
2022-04-29 12:00:56 -0300

    Testes de Enemy criados

--- spec/requests/enemies_spec.rb
+++ spec/requests/enemies_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe "Enemies", type: :request do
     end
   end
 
-  describe "DELETE /enemies" do
+  describe "DELETE /destroy" do
     context "when the enemy exists" do
       let(:enemy) { create(:enemy) }
       before(:each) { delete "/enemies/#{enemy.id}" }
@@ -61,9 +61,48 @@ RSpec.describe "Enemies", type: :request do
     end
   end
 
-  describe "GET /enemies" do # Criar
+  describe "GET /index" do
+    it "returns status code 200" do
+      get enemies_path
+      expect(response).to have_http_status(200)
+    end
   end
 
-  describe "POST /enemies" do # Criar
+  describe "GET /show" do
+    context "when the enemy exists" do
+      let(:enemy) { create(:enemy) }
+      before(:each) { get "/enemies/#{enemy.id}" }
+
+      it "returns status code 200" do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "when the enemy does not exist" do
+      it "returns status code 404" do
+        get "/enemies/0"
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+  describe "POST /create" do # método que permite a criação de novos inimigos e retorna via json os dados do inimigo criado
+    context "when it has valid parameters" do
+      let(:enemy) { create(:enemy) }
+      let(:enemy_attributes) { attributes_for(:enemy) }
+      before(:each) { post "/enemies/", params: enemy_attributes }
+
+      it "create the enemy with correct attributes" do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context "when it has no valid parameters" do
+      it "does not create enemy" do
+        expect{
+          post enemies_path, params: { enemy: { name: 'IsThisSparta?', power_base: 1000, power_step: 100, level: '', kind: '' } }
+        }.to_not change(Enemy, :count)
+      end
+    end
   end
 end
